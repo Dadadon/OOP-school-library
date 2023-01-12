e558d86af363254003bf934cb236a9a93dcfac1b
dadadon <davianberoni@yahoo.com>
2023-01-12 11:21:28 -0500

    Fix linter error

--- Classes/test.rb
+++ Classes/test.rb
@@ -3,9 +3,9 @@ require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
-  person.correct_name
-  capitalizedPerson = CapitalizeDecorator.new(person)
-  capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-  capitalizedTrimmedPerson.correct_name
-  puts person
+person.correct_name
+capitalized_Person = CapitalizeDecorator.new(person)
+capitalized_Person.correct_name
+capitalized_Trimmed_Person = TrimmerDecorator.new(capitalized_Person)
+capitalized_Trimmed_Person.correct_name
+puts person
