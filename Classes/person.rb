3940afef206a51e5fb40fc0809d4b1e22b4e6bc3
dadadon <davianberoni@yahoo.com>
2023-01-12 22:12:56 -0500

    Create the has-many side of Person and Rental

--- Classes/person.rb
+++ Classes/person.rb
@@ -1,20 +1,26 @@
 require 'uuid'
 require_relative 'nameable'
+require_relative 'rental'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = generate_id
     @name = name
     @age = age
+    @rentals = []
     @parent_permission = parent_permission
     super()
   end
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   def correct_name
